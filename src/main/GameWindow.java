c27a4dd4ceccf2f1f1ece9765a2081095d1b3cd6
Soltyssss08 <263495@student.pwr.edu.pl>
2024-06-05 16:08:11 +0200

    New game loop

--- src/main/GameWindow.java
+++ src/main/GameWindow.java
@@ -3,18 +3,19 @@ package main;
 import javax.swing.JFrame;
 
 public class GameWindow {
-    private JFrame jframe;
+	private JFrame jframe;
 
-    public GameWindow(GamePanel gamePanel) {
+	public GameWindow(GamePanel gamePanel) {
 
-        jframe = new JFrame();
+		jframe = new JFrame();
 
-        jframe.setSize(400, 400);
-        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jframe.add(gamePanel);
-        jframe.setLocationRelativeTo(null);
-        jframe.setVisible(true);
+		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jframe.add(gamePanel);
+		jframe.setResizable(false);
+		jframe.pack();
+		jframe.setLocationRelativeTo(null);
+		jframe.setVisible(true);
 
-    }
+	}
 
 }
